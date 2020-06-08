d7a81c424ad1e56897dfa4bd3c1db991172ebfaf
ThomasZB <1090669226@qq.com>
2020-06-08 23:08:49 +0800

    完善了部分函数代码

--- function/my_decode.py
+++ function/my_decode.py
@@ -1,2 +1,33 @@
-def decode_box(predict_result):
-    (boxes, scores, classes, num) = predict_result
+
+import numpy as np
+import tensorflow as tf
+
+
+class decode_reslut():
+    # 获得图像的宽和高
+    # 确定图像的最低分数显示
+    def __init__(self, image_np, min_score):
+        self.im_width = image_np.shape[0]
+        self.im_height = image_np.shape[1]
+        self.min_score = min_score
+
+    def decode_box(self, predict_result):
+        (boxes, scores, classes, num) = predict_result
+        boxes = boxes.numpy()
+        scores = scores.numpy()
+        classes = classes.numpy()
+        num = num.numpy()
+
+        for i in range(num.astype(np.uint32)[0]):
+            boxes[i][0] *= self.im_width
+            boxes[i][2] *= self.im_width
+            boxes[i][1] *= self.im_height
+            boxes[i][3] *= self.im_height
+            if scores[i] >= self.min_score:
+                self.object_num = i
+                break
+        processed_scores = scores[:self.object_num]
+        processed_classes = classes[:self.object_num]
+        processed_boxes = boxes[:self.object_num]
+
+        return (processed_boxes, processed_scores, processed_classes, num)
--- road_damage_detective.py
+++ road_damage_detective.py
@@ -10,7 +10,10 @@ sys.path.append('function')
 from PIL import Image
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+from my_decode import *
 
+
+# 用于将tf1模型打包
 def wrap_frozen_graph(graph_def, inputs, outputs):
   def _imports_graph_def():
     tf.compat.v1.import_graph_def(graph_def, name="")
@@ -21,12 +24,14 @@ def wrap_frozen_graph(graph_def, inputs, outputs):
       tf.nest.map_structure(import_graph.as_graph_element, outputs))
 
 
+# 用于将图像转化为numpy数组
 def load_image_into_numpy_array(image_path):
     image = Image.open(image_path)
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape(im_height, im_width, 3).astype(np.uint8)
 
 
+# 定义相关的错误类型
 category_index = {1: {'id': 1, 'name': 'ShuXiang'},
                     2: {'id': 2, 'name': 'HengXiang'},
                     3: {'id': 3, 'name': 'GuiLie'},
@@ -35,8 +40,8 @@ category_index = {1: {'id': 1, 'name': 'ShuXiang'},
                     6: {'id': 6, 'name': 'ZhongXian'},
                     7: {'id': 7, 'name': 'JinGai'}}
 
-# 载入模型
 
+# 载入模型
 graph_def = tf.compat.v1.GraphDef()
 graph_def.ParseFromString(open('.\model\\frozen_inference_graph_mobilenet.pb','rb').read())
 mobile_func = wrap_frozen_graph(graph_def, 
@@ -44,10 +49,8 @@ mobile_func = wrap_frozen_graph(graph_def,
                                 outputs=('detection_boxes:0', 'detection_scores:0','detection_classes:0', 'num_detections:0'))
 
 # 图片的预处理（进行中值滤波）
-
 processed_image = []
-# 获取不同方位图像的列表
-image_path_list = [filename for filename in os.listdir('.\\save_image')]
+image_path_list = [filename for filename in os.listdir('.\\save_image')] # 获取不同方位图像的列表
 # 对不同目录的图片分别进行滤波
 for image_path in image_path_list:
     file_list = [filename for filename in os.listdir('.\\save_image\\' + image_path)]
@@ -62,7 +65,11 @@ for image_path in image_path_list:
 
 # 同时预测不同方向的图片
 image_tensor = tf.convert_to_tensor(processed_image)
-(boxes, scores, classes, num) = mobile_func(image_tensor)
+result = mobile_func(image_tensor).numpy()
 
 
 # 对输出进行解码
+result_decoded = []
+for image in processed_image:
+
+im_decode = decode_reslut(processed_image[0], 0.3)
