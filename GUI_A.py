02f63ceed544acde4d72557526a3a61a167fb999
ThomasZB <1090669226@qq.com>
2020-06-10 21:45:38 +0800

    更新了GUI界面显示，更新了服务器代码，打包了检测程序

---
+++ GUI_A.py
@@ -0,0 +1,197 @@
+import wx
+import numpy as np
+from road_damage_detective import *
+
+
+class OtherFrame(wx.Frame):
+    # 初始化
+    def __init__(self, parent, id, title, result):
+        # 继承父类的__init__()函数
+        wx.Frame.__init__(self, parent, id, title, size=(1100, 600))
+        self.result = result
+        self.title = title
+        self.list=['无','竖向裂缝   ','横向裂缝   ','地面龟裂   ','坑洼         ','斑马线模糊','白线模糊   ']
+        self.InitUI()
+        self.Centre()
+        self.Show()
+
+    def InitUI(self):
+        panel = wx.Panel(self, -1, size=(1100, 600))
+        sizer = wx.BoxSizer(wx.HORIZONTAL)  # 列间隔为10，行间隔为20
+        # panel.SetBackgroundColour('yellow')
+
+        btn1 = wx.Button(panel, -1, '角度1')
+        sizer.Add(btn1, proportion=0, flag=wx.ALL, border=5)
+        self.Bind(wx.EVT_BUTTON, self.change_picture1, btn1)
+        btn2 = wx.Button(panel, -1, '角度2')
+        sizer.Add(btn2, proportion=0, flag=wx.ALL, border=5)
+        self.Bind(wx.EVT_BUTTON, self.change_picture2, btn2)
+        btn3 = wx.Button(panel, -1, '角度3')
+        sizer.Add(btn3, proportion=0, flag=wx.ALL, border=5)
+        self.Bind(wx.EVT_BUTTON, self.change_picture3, btn3)
+
+        v_sizer = wx.BoxSizer(wx.VERTICAL)
+        # Imfor = wx.TextCtrl(panel, size=(283, 600))
+        v_sizer.Add(sizer, proportion=0, flag=wx.EXPAND)
+        s0='检测到地面异常数量为： '+str(self.result[0][3])
+        self.text0 = wx.StaticText(parent=panel, label=s0)
+        v_sizer.Add(self.text0, proportion=0, flag=wx.ALL, border=5)
+
+        # text.SetForegroundColour("Black")
+        # text.SetBackgroundColour("White")
+        s = "   "
+        if self.result[0][3]>0:
+            s = '具体信息如下'
+            for i in range(self.result[0][3]):
+                s = s + '\n'+ self.list[self.result[0][2][i]] + '        概率为：' + str(self.result[0][1][i])
+
+        self.text1 = wx.StaticText(parent=panel, label=s)
+        v_sizer.Add(self.text1, proportion=0, flag=wx.ALL, border=5)
+
+        h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        image_file = 'predict_image\\0.jpg'
+        image1 = wx.Image(image_file, wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        self.bmp = wx.StaticBitmap(panel, -1, image1)  # 转化为.StaticBitmap()形式
+        h_sizer.Add(v_sizer, proportion=0, flag=wx.EXPAND)
+        h_sizer.Add(self.bmp, proportion=0, flag=wx.ALL, border=5)
+        # 将Panmel适应GridBagSizer()放置
+        self.SetSizer(h_sizer)
+
+    def change_picture1(self, event):
+        if self.title == "0号点":
+            s0='检测到地面异常数量为： '+str(self.result[0][3])
+            self.text0.SetLabel(s0)
+            s = "   "
+            if self.result[0][3] > 0:
+                s = '具体信息如下'
+                for i in range(self.result[0][3]):
+                    s = s + '\n' + self.list[self.result[0][2][i]] + '        概率为：' + str(self.result[0][1][i])
+            self.text1.SetLabel(s)
+            image = wx.Image('predict_image\\0.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "1号点":
+            image = wx.Image('predict_image\\1.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "2号点":
+            image = wx.Image('predict_image\\2.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        self.bmp.SetBitmap(wx.BitmapFromImage(image))
+
+    def change_picture2(self, event):
+        if self.title == "0号点":
+            s0 = '检测到地面异常数量为： ' + str(self.result[1][3])
+            self.text0.SetLabel(s0)
+            s = "   "
+            if self.result[1][3] > 0:
+                s = '具体信息如下'
+                for i in range(self.result[1][3]):
+                    s = s + '\n' + self.list[self.result[1][2][i]] + '        概率为：' + str(self.result[1][1][i])
+            self.text1.SetLabel(s)
+            image = wx.Image('predict_image\\1.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "1号点":
+            image = wx.Image('predict_image\\2.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "2号点":
+            image = wx.Image('predict_image\\0.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        self.bmp.SetBitmap(wx.BitmapFromImage(image))
+
+    # 定义文字及图片转换函数
+    def change_picture3(self, event):
+        # 将文字“北京”换成“广州”
+        # self.text2.SetLabel("广州")
+        # 获取广州的图片，转化为Bitmap形式
+        if self.title == "0号点":
+            s0 = '检测到地面异常数量为： ' + str(self.result[2][3])
+            self.text0.SetLabel(s0)
+            s = "   "
+            if self.result[2][3] > 0:
+                s = '具体信息如下'
+                for i in range(self.result[2][3]):
+                    s = s + '\n' + self.list[self.result[2][2][i]] + '        概率为：' + str(self.result[2][1][i])
+            self.text1.SetLabel(s)
+            image = wx.Image('predict_image\\2.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "1号点":
+            image = wx.Image('predict_image\\1.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        elif self.title == "2号点":
+            image = wx.Image('predict_image\\2.jpg', wx.BITMAP_TYPE_ANY).Rescale(800, 600).ConvertToBitmap()
+        # 更新GridBagSizer()的self.bmp2
+        self.bmp.SetBitmap(wx.BitmapFromImage(image))
+
+
+def show_other0(event):
+    frame = OtherFrame(None, -1, "0号点", result_decoded)
+    frame.Show()
+
+
+def show_other1(event):
+    result_decoded1 = [(np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2),
+                       (np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2),
+                       (np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2)]
+    frame = OtherFrame(None, -1, "1号点", result_decoded1)
+    frame.Show()
+
+
+def show_other2(event):
+    result_decoded2 = [(np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2),
+                       (np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2),
+                       (np.array([0, 697, 587, 879]), np.array([60, 668, 1009, 915]), np.array([0.3550625, 0.30893087]),
+                        np.array([1, 1]), 2)]
+    frame = OtherFrame(None, -1, "2号点", result_decoded2)
+    frame.Show()
+    # f2 = wx.Frame(None, -1, size=(800,600))
+    #
+    # ch1 = wx.ComboBox(panel, -1, value='C#', choices=list1)
+    # self.Bind(wx.EVT_COMBOBOX, self.on_combox, ch1)
+    # hbox1.Add(statictext, 1, flag=wx.LEFT | wx.RIGHT | wx.FIXED_MINSIZE)
+    # hbox1.Add(ch1, 1, flag=wx.ALL | wx.EXPAND)
+    # Cho = wx.Choice(f2, -1, (100, 50), choices=['左边', '前方', '右方'])
+    # f2.Show()
+
+
+result_decoded = image_predict()
+print(result_decoded[0])
+print(result_decoded[1])
+print(result_decoded[2])
+
+
+print(result_decoded[0][3])
+print(result_decoded[1])
+print(result_decoded[2])
+
+level = 0
+color = ["file\\green", "file\\green", "file\\green"]
+for i in range(3):
+    if result_decoded[i][3] > 0:
+        level = result_decoded[i][1].sum()
+        if level > 0.5:
+            color[0] = "file\\red"
+            break
+        elif 0 < level <= 0.5:
+            color[0] = "file\\yellow"
+
+app = wx.App()
+win = wx.Frame(None, title="RoadDamageDetector", size=(1010, 575))
+win.Show()
+image_file = 'file\\image.jpg'
+to_bmp_image = wx.Image(image_file, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+win.bitmap = wx.StaticBitmap(win, -1, to_bmp_image, (0, 0))
+image_width = to_bmp_image.GetWidth()
+image_height = to_bmp_image.GetHeight()
+
+s0 = color[0] + ".bmp"
+bmp0 = wx.Image(s0, wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+button0 = wx.BitmapButton(win.bitmap, -1, bmp0, pos=(450, 170), size=(10, 10))
+s1 = color[1] + ".bmp"
+bmp1 = wx.Image(s1, wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+button1 = wx.BitmapButton(win.bitmap, -1, bmp1, pos=(500, 100), size=(10, 10))
+s2 = color[2] + ".bmp"
+bmp2 = wx.Image(s2, wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+button2 = wx.BitmapButton(win.bitmap, -1, bmp2, pos=(450, 300), size=(10, 10))
+
+button0.Bind(wx.EVT_BUTTON, show_other0)
+button1.Bind(wx.EVT_BUTTON, show_other1)
+button2.Bind(wx.EVT_BUTTON, show_other2)
+
+win.Show()
+app.MainLoop()
--- road_damage_detective.py
+++ road_damage_detective.py
@@ -28,66 +28,68 @@ def wrap_frozen_graph(graph_def, inputs, outputs):
 def load_image_into_numpy_array(image_path):
     image = Image.open(image_path)
     (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(im_height, im_width, 3).astype(np.uint8)
+    return np.array(image.getdata()).reshape(im_height, im_width, 3).astype(np.int32)
 
 
-# 定义相关的错误类型
-category_index = {1: {'id': 1, 'name': 'Vertical crack'},
-                    2: {'id': 2, 'name': 'Transverse crack'},
-                    3: {'id': 3, 'name': 'Cracked ground'},
-                    4: {'id': 4, 'name': 'Pothole'},
-                    5: {'id': 5, 'name': 'Cross walk blur'},
-                    6: {'id': 6, 'name': 'White line blur'},
-                    7: {'id': 7, 'name': 'Manhole cover'}}
+def relu(x):
+    return np.maximum(0, x)
 
 
-# 载入模型
-graph_def = tf.compat.v1.GraphDef()
-graph_def.ParseFromString(open('.\model\\frozen_inference_graph_mobilenet.pb','rb').read())
-mobile_func = wrap_frozen_graph(graph_def, 
-                                inputs='image_tensor:0', 
-                                outputs=('detection_boxes:0', 'detection_scores:0','detection_classes:0', 'num_detections:0'))
-
-# 图片的预处理（进行中值滤波）
-processed_image = []
-image_path_list = [filename for filename in os.listdir('.\\save_image')]  # 获取不同方位图像的列表
-# 对不同目录的图片分别进行滤波
-for image_path in image_path_list: 
-    file_list = [filename for filename in os.listdir('.\\save_image\\' + image_path)]
-    img_list = []
-    for file in file_list:
-        img = cv2.imread('save_image\\' + image_path + '\\' + file)
-        img_list.append(img)
-    last_img = np.median(img_list, axis=0).astype(np.uint8)
-    processed_image.append(last_img)
-
-processed_image = np.array(processed_image).astype(np.uint8)
-
-# 模型的预测
-image_tensor = tf.convert_to_tensor(processed_image)  # 同时预测不同方向的图片
-predict_result = mobile_func(image_tensor)
-
-# 对输出进行解码
-result_decoded = []
-result_image = []
-for i in range(3):
-    tmp_result = (predict_result[0][i], predict_result[1][i], predict_result[2][i], predict_result[3][i])
-    im_decode = decode_reslut(processed_image[i], 0.25)
-    result_decoded.append(im_decode.decode_box(tmp_result))
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        processed_image[i],
-        np.squeeze(tmp_result[0]),
-        np.squeeze(tmp_result[2]).astype(np.int32),  # class
-        np.squeeze(tmp_result[1]),
-        category_index,
-        min_score_thresh=0.25,
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    im = Image.fromarray(processed_image[i])
-    im.save("predict_image\\" + str(i) + ".jpeg")
+def image_predict():
+    # 定义相关的错误类型
+    category_index = {1: {'id': 1, 'name': 'Vertical crack'},
+                        2: {'id': 2, 'name': 'Transverse crack'},
+                        3: {'id': 3, 'name': 'Cracked ground'},
+                        4: {'id': 4, 'name': 'Pothole'},
+                        5: {'id': 5, 'name': 'Cross walk blur'},
+                        6: {'id': 6, 'name': 'White line blur'},
+                        7: {'id': 7, 'name': 'Manhole cover'}}
 
 
-# 打印结果
-print(result_decoded[0])
-print(result_decoded[1])
-print(result_decoded[2])
+    # 载入模型
+    graph_def = tf.compat.v1.GraphDef()
+    graph_def.ParseFromString(open('.\model\\frozen_inference_graph_mobilenet.pb','rb').read())
+    mobile_func = wrap_frozen_graph(graph_def, 
+                                    inputs='image_tensor:0', 
+                                    outputs=('detection_boxes:0', 'detection_scores:0','detection_classes:0', 'num_detections:0'))
+
+    # 图片的预处理（进行中值滤波）
+    processed_image = []
+    image_path_list = [filename for filename in os.listdir('.\\save_image')]  # 获取不同方位图像的列表
+    substracted = load_image_into_numpy_array('file\\test.bmp')
+    # 对不同目录的图片分别进行滤波
+    for image_path in image_path_list: 
+        file_list = [filename for filename in os.listdir('.\\save_image\\' + image_path)]
+        img_list = []
+        for file in file_list:
+            img = load_image_into_numpy_array('save_image\\' + image_path + '\\' + file)
+            img = relu(np.subtract(img, substracted)).astype(np.uint8)
+            img_list.append(img)
+        last_img = np.median(img_list, axis=0).astype(np.uint8)
+        processed_image.append(last_img)
+
+    processed_image = np.array(processed_image).astype(np.uint8)
+
+    # 模型的预测
+    image_tensor = tf.convert_to_tensor(processed_image)  # 同时预测不同方向的图片
+    predict_result = mobile_func(image_tensor)
+
+    # 对输出进行解码
+    result_decoded = []
+    for i in range(3):
+        tmp_result = (predict_result[0][i], predict_result[1][i], predict_result[2][i], predict_result[3][i])
+        im_decode = decode_reslut(processed_image[i], 0.2)
+        result_decoded.append(im_decode.decode_box(tmp_result))
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            processed_image[i],
+            np.squeeze(tmp_result[0]),
+            np.squeeze(tmp_result[2]).astype(np.int32),  # class
+            np.squeeze(tmp_result[1]),
+            category_index,
+            min_score_thresh=0.2,
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        im = Image.fromarray(processed_image[i])
+        im.save("predict_image\\" + str(i) + ".jpg")
+    return result_decoded
+
